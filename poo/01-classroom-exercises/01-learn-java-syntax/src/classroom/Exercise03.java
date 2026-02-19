5668729089b6780665d6250cf48fb182c97aea09
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-02-19 18:42:02 -0300

    FEAT: Make the exercise 03, print the multiple of 3

---
+++ poo/01-classroom-exercises/01-learn-java-syntax/src/classroom/Exercise03.java
@@ -0,0 +1,13 @@
+package classroom;
+
+/*03 - Imprima todos os múltiplos de 3, entre 1 e 100*/
+
+public class Exercise03 {
+    static void main() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0){
+                System.out.printf("O numero %d eh multiplo de 3\n", i);
+            }
+        }
+    }
+}
