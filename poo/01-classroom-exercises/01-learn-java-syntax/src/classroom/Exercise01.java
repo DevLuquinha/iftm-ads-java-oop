ddcc9e53b8012164bd0f61ad8fd3f03e800ff143
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-02-19 18:31:01 -0300

    FEAT: Make the exercise 01, print 150 to 300

--- poo/01-classroom-exercises/01-learn-java-syntax/src/classroom/Exercise01.java
+++ poo/01-classroom-exercises/01-learn-java-syntax/src/classroom/Exercise01.java
@@ -1,10 +1,11 @@
 package classroom;
-
 /*
 01 - Imprima todos os números de 150 a 300.
 */
 public class Exercise01{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+    static void main(String[] args){
+        for (int i = 150; i<= 300; i++){
+            System.out.printf("Numero: %d\n", i);
+        }
     }
 }
