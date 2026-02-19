dcf0618bec684c478722065abed09af1c177bc6d
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-02-19 18:37:18 -0300

    FEAT: Make the exercise 02, sum that 1 to 1000

---
+++ poo/01-classroom-exercises/01-learn-java-syntax/src/classroom/Exercise02.java
@@ -0,0 +1,14 @@
+package classroom;
+/*
+02 - Imprima a soma de 1 até 1000.
+*/
+public class Exercise02 {
+    static void main(String[] args){
+        int sum = 0;
+        for (int i = 1; i <= 1000; i++) {
+            sum += i;
+        }
+
+        System.out.printf("The sum that 1 to 1000 is: " + sum);
+    }
+}
