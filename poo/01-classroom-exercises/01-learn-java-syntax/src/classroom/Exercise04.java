96fdf0730d52cd584add5d49052aa2a93eeaf7a1
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-02-19 18:54:11 -0300

    FEAT: Make the exercise 04, print the factorial number

---
+++ poo/01-classroom-exercises/01-learn-java-syntax/src/classroom/Exercise04.java
@@ -0,0 +1,30 @@
+package classroom;
+/*04 - Imprima os fatoriais de 1 a 10.*/
+
+public class Exercise04 {
+    static void main() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf(
+                    "O fatorial de %d eh (%d!) * %d = %d\n",
+                    i,
+                    i,
+                    i+1,
+                    factorial(i)
+            );
+        }
+    }
+
+    static int factorial(int num){
+        if (num == 0){
+            return 1;
+        }
+
+        int result = num;
+
+        for (int i = num; i >= 2; i--) {
+            result = result * (i-1);
+        }
+
+        return result;
+    }
+}
