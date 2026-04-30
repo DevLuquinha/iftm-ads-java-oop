7ae2584bd771e0d6b33412925515759b2741d5cf
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-30 20:34:20 -0300

    Learning about exceptions checked and unchecked

---
+++ oop/01-classroom-exercises/src/classroom/class_26_04_30/MissingInventoryUncheckedException.java
@@ -0,0 +1,7 @@
+package classroom.class_26_04_30;
+
+public class MissingInventoryUncheckedException extends RuntimeException {
+    public MissingInventoryUncheckedException(String message) {
+        super(message);
+    }
+}
