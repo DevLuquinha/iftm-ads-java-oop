303e1d4c34031cc91500ec150e89b87ecb8f64d4
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-12 21:09:21 -0300

    Create the Vertex class in java

--- searching-sorting/01-classroom-exercises/src/classroom/class_26_05_12/Vertex.java
+++ searching-sorting/01-classroom-exercises/src/classroom/class_26_05_12/Vertex.java
@@ -1,4 +1,26 @@
 package classroom.class_26_05_12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vertex {
+    private String name;
+    private List<Edge> adjacents;
+
+    public Vertex(String name) {
+        this.name = name;
+        this.adjacents = new ArrayList<Edge>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Edge> getAdjacents() {
+        return adjacents;
+    }
+
+    public void addAdjacent(Edge edge) {
+        adjacents.add(edge);
+    }
 }
