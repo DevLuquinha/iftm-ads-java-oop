75240eb48ceafeced0e5d96edc22b818332c483c
DevLuquinha <lucasemmanuelrodrigues737@gmail.com>
2026-06-17 20:28:03 -0300

    Learning about enums in java

---
+++ src/oop/classroom_exercises/classroom/class_26_06_17/Main.java
@@ -0,0 +1,11 @@
+package oop.classroom_exercises.classroom.class_26_06_17;
+
+public class Main {
+    static void main(String[] args) {
+        Probability probability = Probability.SEVEN;
+        System.out.println(probability.getProbabilityValue());
+
+        probability = Probability.EIGHT;
+        System.out.println(probability.getProbabilityValue());
+    }
+}
